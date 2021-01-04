730afb14e37631aea83cb645921fa767fd1c47d4
sapirg50 <sapirg50@users.noreply.github.com>
2021-01-04 02:34:53 +0200

    commit generate_report - done

--- ex_4.py
+++ ex_4.py
@@ -1,8 +1,9 @@
 import math
 import sys
-
+import matplotlib.pyplot as plt
 import torch
 from numpy import ndarray
+from scipy.io import wavfile
 from torchvision import transforms, datasets
 import numpy as np
 
@@ -10,6 +11,8 @@ from fashion_dataset import StdNormalizer, FashionDataset
 from model_a import ModelA
 from model_wrapper import ModelWrapper
 from torch.utils.data import random_split
+
+colors = iter(plt.rcParams['axes.prop_cycle'].by_key()['color'])
 
 MODELS = {
     'A': ModelA
@@ -56,7 +59,41 @@ def run_model(train_x_path: str, train_y_path: str, test_x_path: str, model_name
     with open('test_y', 'w') as file:
         for pred in predict.numpy():
             file.write(f'{pred}\n')
-    return
+    return (loss_arr, accuracy_arr), (loss, accuracy)
+
+
+def generate_report(train_x_path: str, train_y_path: str, test_x_path: str, num_of_rows: int = None):
+    model_list = list(MODELS.keys())
+    epochs = 15
+    accuracy_training = []
+    loss_training = []
+    accuracy_validation = []
+    loss_validation = []
+    mode = ['loss', 'accuracy']
+    # training graphs
+    for model in model_list:
+        training, validation = run_model(train_x_path, train_y_path, test_x_path, model, epochs, 0.1, 500, num_of_rows)
+        loss_training.append(np.array(training[0]))
+        accuracy_training.append(np.array(training[1]))
+        loss_validation.append(validation[0])
+        accuracy_validation.append(validation[1])
+    train = [loss_training, accuracy_training]
+    vali = [loss_validation, accuracy_validation]
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12), dpi=500)
+    axs = axs.flatten()
+    fig.suptitle('Results', fontsize=20)
+    # validation graphs
+    for i in range(2):
+        axs[i].set_title(f'$Training$ ${mode[i]}$')
+        axs[i].set_xlabel('$Iterations$')
+        axs[i].set_ylabel(f'$Average$ ${mode[i]}$')
+        for model_index, color in zip(range(len(model_list)), colors):
+            axs[i].plot(train[i][model_index], c=color, lw=2, label=model_list[model_index])
+        axs[i].legend(ncol=4, fontsize='large', loc='best')
+    for i in range(2, 4):
+        axs[i].set_title(f'$Validation$ ${mode[i % 2]}$')
+        axs[i].bar(model_list, vali[i % 2])
+    plt.savefig('plot.jpeg')
 
 
 def main(train_x_path: str, train_y_path: str, test_x_path: str, num_of_rows: int = None):
@@ -81,4 +118,5 @@ if __name__ == '__main__':
     # print(diff_rows_list)
     NUM_OF_ROWS = None
     if len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], NUM_OF_ROWS)
+        # main(sys.argv[1], sys.argv[2], sys.argv[3], NUM_OF_ROWS)
+        generate_report(sys.argv[1], sys.argv[2], sys.argv[3], NUM_OF_ROWS)
